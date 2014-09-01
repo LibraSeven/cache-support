dc9510bc67cb3cd33697ddcc519ddcfa651d7f67
sunyamengbuhtig <sunyameng_anis@sina.com>
2014-09-01 09:12:38 +0800

    rename demo1

--- httpclientdemo/src/main/java/com/demo/httpclient/ClientConnectionRelease.java
+++ httpclientdemo/src/main/java/com/demo/httpclient/ClientConnectionRelease.java
@@ -1,6 +1,5 @@
 package com.demo.httpclient;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
