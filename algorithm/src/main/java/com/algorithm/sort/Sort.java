f4cb66bc209661e96e8746ff1bda4a4c275f572c
yamorn <yamorn@outlook.com>
2015-03-06 10:26:00 +0800

    recursive ignore idea target

--- algorithm/src/main/java/com/algorithm/sort/Sort.java
+++ algorithm/src/main/java/com/algorithm/sort/Sort.java
@@ -4,6 +4,18 @@ package com.algorithm.sort;
  * Created by louis on 2015/3/5.
  */
 public class Sort {
+    public static void selectionSort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[min] > data[j]) {
+                    min = j;
+                }
+            }
+            swap(data, i, min);
+        }
+    }
+
     public static void insertionSort(int[] data) {
         if (data.length == 0)
             return;
@@ -23,21 +35,27 @@ public class Sort {
         if (data.length == 0)
             return;
         for (int i = 0; i < data.length - 1; i++) {
-            for (int j = 1; j < data.length-i; j++) {
-                if (data[j-1] > data[j]) {
-                    int temp = data[j-1];
-                    data[j-1] = data[j];
-                    data[j] = temp;
+            for (int j = 1; j < data.length - i; j++) {
+                if (data[j - 1] > data[j]) {
+                    swap(data, j - 1, j);
                 }
             }
         }
+    }
+
+    public static void swap(int[] data, int i, int j) {
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
     }
 
 
     public static void main(String[] args) {
         int[] data = {5, 2, 4, 6, 1, 3};
 //        Sort.insertionSort(data);
-        Sort.bubbleSort(data);
+//        Sort.bubbleSort(data);
+        Sort.selectionSort(data);
+
 
         Sort.print(data);
     }
